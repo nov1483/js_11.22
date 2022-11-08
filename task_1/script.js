5a72ad62e062c168ad549718a561c4aac3c2c844
Anton <novickii8314@gmail.com>
2022-11-08 19:39:06 +0100

    all tasks fixed

--- task_1/script.js
+++ task_1/script.js
@@ -1,14 +1,14 @@
 function mathWithPrompt(){
-    let firstNumber = prompt('Enter First Number');
-    let secondNumber = prompt('Enter Second Number');
-    firstNumber = Number(firstNumber);
-    secondNumber = Number(secondNumber);
-    if(/^(0|[1-9]\d*)(\.[0-9]{1,2})?$/.test(firstNumber) && /^(0|[1-9]\d*)(\.[0-9]{1,2})?$/.test(secondNumber)){
-        return console.log(
-            `First Number: ${firstNumber}.Second Number: ${secondNumber}.Sum:${firstNumber + secondNumber}.Product: ${firstNumber * secondNumber}.Power:${firstNumber ** secondNumber}`
-        )
-    }
-    return console.error('Incorrect input!')
-    
+  let firstNumber = prompt('Enter First Number');
+  let secondNumber = prompt('Enter Second Number');
+  firstNumber = Number(firstNumber);
+  secondNumber = Number(secondNumber);
+  if(/^(0|[1-9]\d*)(\.[0-9]{1,2})?$/.test(firstNumber) && /^(0|[1-9]\d*)(\.[0-9]{1,2})?$/.test(secondNumber)){
+       console.log(
+          `First Number: ${firstNumber}.Second Number: ${secondNumber}.Sum:${firstNumber + secondNumber}.Product: ${firstNumber * secondNumber}.Power:${firstNumber ** secondNumber}`
+      )
+  }else {
+      console.error('Incorrect input!');
+  }
 }
 mathWithPrompt()
