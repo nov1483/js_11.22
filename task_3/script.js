8c8076390634267c2a3db40ebcfe21f5094cd3e9
Anton <novickii8314@gmail.com>
2022-11-08 19:37:48 +0100

    task_3 fixed

--- task_3/script.js
+++ task_3/script.js
@@ -1,61 +1,62 @@
 function colderWarmer(){
-    let firstNumber = prompt('Enter first number');
-    let secondNumber = prompt('Enter second number');
-    firstNumber = Number(firstNumber);
-    secondNumber = Number(secondNumber);
-    while(isNaN(firstNumber) && isNaN(secondNumber) || firstNumber === null || secondNumber === null){
-        alert('Enter only numbers please!')
-        firstNumber = prompt('Enter first number');
-        secondNumber = prompt('Enter second number');
-        firstNumber = Number(firstNumber);
-        secondNumber = Number(secondNumber);
-    }
-    if(firstNumber < 0 || secondNumber < 0){
-        alert('Please enter only positive numbers');
-        return;
-    }else if((firstNumber + 100) > secondNumber){
-        alert(`Please enter a number greater than ${firstNumber + 100}`);
-        return;
-    }
-    let randomNum = firstNumber + Math.floor(Math.random() * (secondNumber + 1 - firstNumber));
-    let userNum = prompt('What number i guess?');
-    userNum = Number(userNum);
-    if(userNum !== randomNum){
-        alert('Cold!');
-    }
-    if(userNum === randomNum) {
-        alert('Great! It’s like you knew the number');
-        return;
-    }
-    let chance = 1;
-    let lastNumber = []; 
-    lastNumber.push(userNum)
-    while(true){
-        chance++;
-        userNum = prompt('What number i guess?');
-        userNum = Number(userNum);
-        lastNumber.push(userNum);
-        if(userNum  < randomNum &&  userNum > lastNumber[lastNumber.length - 2]){
-            alert('Warmer!')
-        }
-        if(userNum  < randomNum && userNum < lastNumber[lastNumber.length - 2]){
-            alert('Colder!')
-        }
-        if(userNum  > randomNum &&  userNum < lastNumber[lastNumber.length - 2]){
-            alert('Warmer!')
-        }
-        if(userNum > randomNum &&  userNum > lastNumber[lastNumber.length - 2]){
-            alert('Colder!')
-        }
-        if((userNum + 1 ) === randomNum || (userNum - 1) === randomNum){
-            alert('You’re almost there')
-        }
-        if(userNum === randomNum){
-            alert(`You did it in ${chance} attempts. Congratulations!`);
-            break;
-        }
-    }
-    
-    
+  let firstNumber = prompt('Enter first number');
+  let secondNumber = prompt('Enter second number');
+  firstNumber = Number(firstNumber);
+  secondNumber = Number(secondNumber);
+  while(isNaN(firstNumber) || isNaN(secondNumber) || firstNumber === null || secondNumber === null || firstNumber < 0 || secondNumber < 0 || (firstNumber + 100) > secondNumber){
+      firstNumber < 0 || secondNumber < 0 
+          ? 
+              alert('Please enter only positive numbers!') 
+          : 
+      (firstNumber + 100) > secondNumber 
+          ? 
+              alert(`Please enter a number greater than ${firstNumber + 100}`)
+          :
+              alert('Enter only numbers please!');
+      firstNumber = prompt('Enter first number');
+      secondNumber = prompt('Enter second number');
+      firstNumber = Number(firstNumber);
+      secondNumber = Number(secondNumber);
+  }
+  const randomNum = firstNumber + Math.floor(Math.random() * (secondNumber + 1 - firstNumber));
+  let userNum = prompt('What number i guess?');
+  userNum = Number(userNum);
+  if(userNum !== randomNum){
+      alert('Cold!');
+  }
+  if(userNum === randomNum) {
+      alert('Great! It’s like you knew the number');
+      return;
+  }
+  let chance = 1;
+  const lastNumber = []; 
+  lastNumber.push(userNum)
+  while(true){
+      chance++;
+      userNum = prompt('What number i guess?');
+      userNum = Number(userNum);
+      lastNumber.push(userNum);
+      if(userNum  < randomNum &&  userNum > lastNumber[lastNumber.length - 2]){
+          alert('Warmer!')
+      }
+      if(userNum  < randomNum && userNum < lastNumber[lastNumber.length - 2]){
+          alert('Colder!')
+      }
+      if(userNum  > randomNum &&  userNum < lastNumber[lastNumber.length - 2]){
+          alert('Warmer!')
+      }
+      if(userNum > randomNum &&  userNum > lastNumber[lastNumber.length - 2]){
+          alert('Colder!')
+      }
+      if((userNum + 1 ) === randomNum || (userNum - 1) === randomNum){
+          alert('You’re almost there')
+      }
+      if(userNum === randomNum){
+          alert(`You did it in ${chance} attempts. Congratulations!`);
+          break;
+      }
+  }
+  
+  
 }
 colderWarmer();
